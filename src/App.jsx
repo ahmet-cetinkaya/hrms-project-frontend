7a5a8a700db5ce049d94c343c41ac9429ad9aef5
Ahmet Çetinkaya <ahmet4cetinkaya@outlook.com>
2021-06-29 18:28:54 +0300

    ✨Job Adverts Verify Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Footer from "./layouts/Footer/Footer";
 import Home from "./pages/Home/Home";
 import JobAdvertsAdd from "./pages/JobAdverts/Add/JobAdvertsAdd";
+import JobAdvertsVerify from "./pages/JobAdverts/Verify/JobAdvertsVerify";
 import Navbar from "./layouts/Navbar/Navbar";
 import { ToastContainer } from "react-toastify";
 
@@ -19,6 +20,11 @@ function App() {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/jobadverts/add' component={JobAdvertsAdd} />
+            <Route
+              exact
+              path='/jobadverts/verify'
+              component={JobAdvertsVerify}
+            />
           </Switch>
         </main>
         <Footer />
