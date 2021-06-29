9a5d82db141a0cb09c1f00c0e0b9b8b0fb389a0a
Ahmet Çetinkaya <ahmet4cetinkaya@outlook.com>
2021-06-29 18:24:12 +0300

    ✨ Toast Notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import Navbar from './layouts/Navbar/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "bootstrap";
+import "./App.scss";
+import "react-toastify/dist/ReactToastify.css";
 
-import Home from './pages/Home/Home';
-import Footer from './layouts/Footer/Footer';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import 'bootstrap';
-import './App.scss';
+import Footer from "./layouts/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Navbar from "./layouts/Navbar/Navbar";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div className='App'>
+    <div id='App' className='d-flex flex-column'>
       <Router>
         <Navbar />
-        <Switch>
-          <Route path='/' component={Home} />
-        </Switch>
+        <main className='flex-auto'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </main>
         <Footer />
+        <ToastContainer
+          position='bottom-right'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </Router>
     </div>
   );
